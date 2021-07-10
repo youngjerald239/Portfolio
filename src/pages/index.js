3f9611da62cee1f0eef9ff52ba96660db0a3d530
youngjerald239 <youngjerald239@gmail.com>
2021-07-09 20:59:02 -0500

    updated navbar to fixed position and added avatar

--- src/pages/index.js
+++ src/pages/index.js
@@ -132,7 +132,7 @@ focus on the user in all of my projects.
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={"https://scontent-ort2-2.xx.fbcdn.net/v/t39.1997-6/p160x160/98276302_3092600417468899_688522178542436352_n.png?_nc_cat=110&ccb=1-3&_nc_sid=ac3552&_nc_ohc=MS9wwtZilW8AX_FH5zm&_nc_ht=scontent-ort2-2.xx&tp=30&oh=ff5aa2b3c4263d6ba9752ba56103b093&oe=60D0CC6F"} className="img-fluid" alt="avatar" />
+              <img src={"https://sdk.bitmoji.com/render/panel/cf7648af-2035-4e07-9237-89aa82985b3c-7dfb92cb-680a-40d6-b1a1-ae0ce8e6f183-v1.png?transparent=1&palette=1"} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
